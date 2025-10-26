972893de3a0ee9a940017e088218f3ef3227cacd
fstewart-collab <fstewart@gracechristian.net>
2025-10-26 13:09:07 -0400

    Update shootMode.js

--- js/shootMode.js
+++ js/shootMode.js
@@ -1,18 +1,43 @@
 let loopInterval;
+let countdownInterval;
 let showingQuizlet = true;
+let timeLeft = 10;
 
 function startShootMode(deckName, decks) {
   const gameArea = document.getElementById("gameArea");
-  gameArea.innerHTML = `<button id="stopLoopBtn">🛑 Stop</button>`;
-  const stopBtn = document.getElementById("stopLoopBtn");
 
-  stopBtn.onclick = stopGameLoop;
+  function showTimer() {
+    const timer = document.createElement("div");
+    timer.id = "timerDisplay";
+    timer.textContent = `Next switch in: ${timeLeft}s`;
+    gameArea.appendChild(timer);
+  }
+
+  function updateTimer() {
+    const timer = document.getElementById("timerDisplay");
+    if (timer) timer.textContent = `Next switch in: ${timeLeft}s`;
+  }
+
+  function startCountdown() {
+    clearInterval(countdownInterval);
+    timeLeft = 10;
+    updateTimer();
+    countdownInterval = setInterval(() => {
+      timeLeft--;
+      updateTimer();
+      if (timeLeft <= 0) {
+        toggleContent();
+      }
+    }, 1000);
+  }
 
   function showQuizlet() {
     gameArea.innerHTML = `
       <button id="stopLoopBtn">🛑 Stop</button>
       <iframe src="https://quizlet.com/latest" class="iframeBox"></iframe>
     `;
+    showTimer();
+    startCountdown();
   }
 
   function showBasketball() {
@@ -20,19 +45,27 @@ function startShootMode(deckName, decks) {
       <button id="stopLoopBtn">🛑 Stop</button>
       <iframe src="https://basketball-stars.io" class="iframeBox"></iframe>
     `;
+    showTimer();
+    startCountdown();
   }
 
   function toggleContent() {
-    if (showingQuizlet) showBasketball();
-    else showQuizlet();
     showingQuizlet = !showingQuizlet;
+    if (showingQuizlet) showQuizlet();
+    else showBasketball();
   }
 
+  // initial view
   showQuizlet();
-  loopInterval = setInterval(toggleContent, 10000); // 10s each
+
+  // stop button
+  document.body.addEventListener("click", e => {
+    if (e.target.id === "stopLoopBtn") stopGameLoop();
+  });
 }
 
 function stopGameLoop() {
   clearInterval(loopInterval);
+  clearInterval(countdownInterval);
   location.reload();
 }
