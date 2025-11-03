d0b34873bbe9096a12709329c0a65915aca2e39d
fstewart-collab <fstewart@gracechristian.net>
2025-11-03 09:39:54 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -89,7 +89,6 @@ function startGame(link) {
     timeLeft--;
     timer.textContent = `Next Quizlet in: ${timeLeft}s`;
     if (timeLeft <= 0) {
-      console.log("gameover");
       clearInterval(interval);
       startStudy(link);
     }
